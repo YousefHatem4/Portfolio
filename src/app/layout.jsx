72d4d6dd9b3e0a4d44227ab8dcf32928231bb984
YousefHatem4 <youssefhatem242004@gmail.com>
2025-04-06 23:52:52 +0200

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,7 @@ export default function RootLayout({ children }) {
   return <>
     <html>
       <body>
-        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Yousef</span>
